4459b43bf4d507eff049d6637ed1ecfa21946f80
hakai-here <syamsv01@gmail.com>
2025-06-14 02:50:39 +0530

    Fix on correction mechanism

--- pkg/imaged/4_convert.go
+++ pkg/imaged/4_convert.go
@@ -1,7 +1,6 @@
 package imaged
 
 import (
-	"bufio"
 	"fmt"
 	"os/exec"
 	"regexp"
@@ -16,8 +15,7 @@ func (q *Qcow2Imaged) Convert() error {
 	if _, err := exec.LookPath("qemu-img"); err != nil {
 		return fmt.Errorf("qemu-img not found in PATH")
 	}
-	cmd := exec.Command("qemu-img", "convert", "-p", "-f", "vmdk", "-O", "qcow2", q.Get7zFileLocation(), q.GetQcow2FileLocation())
-
+	cmd := exec.Command("qemu-img", "convert", "-p", "-f", "vmdk", "-O", "qcow2", q.GetVmdkFileLocation(), q.GetQcow2FileLocation())
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
 		return err
@@ -30,22 +28,25 @@ func (q *Qcow2Imaged) Convert() error {
 	done := make(chan struct{})
 	go func() {
 		defer close(done)
-		scanner := bufio.NewScanner(stderr)
-		re := regexp.MustCompile(`(\d+)%`)
-		for scanner.Scan() {
-			line := scanner.Text()
-			matches := re.FindStringSubmatch(line)
-			if len(matches) > 1 {
-				var percent float64
-				if _, err := fmt.Sscanf(matches[1], "%f", &percent); err == nil {
-					q.mu.Lock()
-					q.Percentage = percent
-					q.mu.Unlock()
+		buf := make([]byte, 1024)
+		re := regexp.MustCompile(`\((\d+\.\d+)/100%\)`)
+		for {
+			n, err := stderr.Read(buf)
+			if n > 0 {
+				output := string(buf[:n])
+				matches := re.FindStringSubmatch(output)
+				if len(matches) > 1 {
+					var percent float64
+					if _, err := fmt.Sscanf(matches[1], "%f", &percent); err == nil {
+						q.mu.Lock()
+						q.Percentage = percent
+						q.mu.Unlock()
+					}
 				}
 			}
-		}
-		if err := scanner.Err(); err != nil {
-			fmt.Println("Scanner error:", err)
+			if err != nil {
+				break
+			}
 		}
 	}()
 
